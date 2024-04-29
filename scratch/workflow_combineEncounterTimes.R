4a92c4130d5767aecf8f3f4884afa8a6d01abef0
sfregosi <22456126+sfregosi@users.noreply.github.com>
2024-04-29 16:14:22 -0700

    operational for all phase 3 trips

--- scratch/workflow_combineEncounterTimes.R
+++ scratch/workflow_combineEncounterTimes.R
@@ -7,6 +7,7 @@ library(here)
 path_picks <- '//piccrp4nas/grampus/llharp/analysis/manualpicks'
 
 # Set trips to merge
+# starting with just phase 3 trips
 trips <- sprintf("LL%03d", seq(60,70))
 
 # set up output data.frame
@@ -14,11 +15,28 @@ allPicks <- data.frame()
 
 # loop through each trip
 for (t in 1:length(trips)){
+  # t <- 1 # for testing
   # trip num
   tripStr <- trips[t]
   # find .csvs
-  dir(file.path(path_picks, tripStr, '*.csv'))
+  encFileList <- list.files(path = file.path(path_picks, tripStr), pattern = '\\.csv$')
+  for (e in 1:length(encFileList)){
+    encFile <- encFileList[e]
+    tmp <- read.csv(file.path(path_picks, tripStr, encFile))
+    allPicks <- rbind(allPicks, tmp)
+  }
   
-}
-# read in first one
-tmp <- read.csv(file.path(path_picks, tripStr))
+} # end trips loop
+
+# remove any duplicates
+# (there shouldn't be any but in case something got read in twice)
+allPicks <- unique(allPicks)
+# clean up NAs in the notes so they don't print when saving to csv
+allPicks$notes[is.na(allPicks$notes)] <- ''
+
+
+# save to csv
+write.csv(allPicks, here('data', paste0('allPicks_phase3_', Sys.Date(), '.csv')))
+
+
+
