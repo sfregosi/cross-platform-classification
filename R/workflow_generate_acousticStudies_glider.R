1aa85045253518aa04d7e4822688ebc52211b296
sfregosi <22456126+sfregosi@users.noreply.github.com>
2024-05-16 16:38:45 -0700

    mods for gliders

--- R/workflow_generate_acousticStudies_glider.R
+++ R/workflow_generate_acousticStudies_glider.R
@@ -1,15 +1,15 @@
 ## Workflow for generating PAMpal AcousticStudy objects for glider data
 # 
 # Requires manually marked cetacean events (from Triton and simplified into an 
-# 'LL000_encounterTimes.csv')
+# a csv)
 #
 # Creates several output .rda files:
-#   'pamVer_LL###_FR##_DL##_params.rda': parameters (pps) input for PAMpal 
-#   'pamVer_LL###_FR##_DL##_dets.rda': 'dets' AcousticStudy with all all 
-#                                     detection feature measurements by event
+#   'pamVer_***_params.rda': parameters (pps) input for PAMpal 
+#   'pamVer_***_dets.rda': 'dets' AcousticStudy with all all detection feature 
+#                           measurements by event
 #
 # Entire Process Overview
-# 1a. Manually identify start/end times of possible odontocete events 
+# 1a. Manually identify start/end times of possible odontocete events in Triton
 # 1b. Run Pamguard click, WM, cepstrum detector
 # 2. [THIS SCRIPT] Generate AcousticStudy - define params, calc features, apply
 #    filter, output 'dets' and 'detsFilt'
@@ -31,48 +31,22 @@ devtools::install_github('taikisan21/PAMpal')
 
 library(here)
 library(PAMpal)
+library(tidyverse)
 
 # ------ USER DEFINED INPUTS ----------------------------------------------
 
-# ------- >>TRIP/RECORDER INFO --------------------------------------------
-# define trip and recorder frame
-tripStr <- 'LL070'
-# recStr <- 'FR04_DL88'
-# recStr <- 'FR05_DL96'
-# recStr <- 'FR10_DL73'
-# recStr <- 'FR10_DL63'
-# recStr <- 'FR38_DL65'
-# recStr <- 'FR45_DL60'
-# recStr <- 'FR45_DL84'
-# recStr <- 'FR51_DL64'
-# recStr <- 'FR52_DL59'
-# recStr <- 'FR63_DL73'
-# recStr <- 'FR63_DL84'
-recStr <- 'FR64_DL88'
-# Pamguard version
+mission <- 'sg639_MHI_Apr2023'
 pgVer <- '20207b'
 
+# define transfer function
+calFile <- 0 # 'C:/path/cal.csv'; to skip calibration set to 0
 
-# ------- >>rarely modified inputs ----------------------------------------
-# ### define transfer function ###
-# calFile <- 0         # for no calibration
-# OLD NOTE: for LL050 onward...use this one - this is flat at 74 dB
-# calFile <- '//piccrp4nas/grampus/llharp/calibration/transfer_functions/RevN_PAMpal_invSensit.csv'
-# more recent from S. Wiggins for Phase 3
-# calFile <- '//piccrp4nas/grampus/llharp/calibration/RevN_type4_110516-07_manual.csv'
-# older deployments will vary by datalogger
-# calFile <- '//picqueenfish/psd2/crp/LLHARP/calibration/transfer_functions/longline_old/DL65/699_PAMr_invSensit.csv'
+# define paths
+path_analysis <- 'T:/glider_MHI_analysis'
+# path_pg <- 'T:/glider_MHI_analysis/pamguard'
+# path_out <- 'T:/glider_MHI_analysis/classification'
 
-# ### define paths ###
-# path_analysis should point to the folder above the 'pamguard' folder (that 
-#               contains the 'databases' and 'binaries' subdirectories), must
-#               contain a 'manualPicks' folder of event .csvs, a 'banterModels' 
-#               folder with models for prediction, and an 'eventData folder
-#               for outputs
-path_analysis <- '//piccrp4nas/grampus/llharp/analysis/'
-# path_llamp <- dirname(dirname(rstudioapi::getSourceEditorContext()$path))
-path_llamp <- here()
-# path_crptools <- file.path(dirname(path_llamp), 'crptools') # assumes same as llamp
+path_llamp <- 'Z:/LLHARP/processingCode/llamp' # bc pull some functions from llamp
 
 # ### configurable PAMpal settings - typically DO NOT change ###
 sr_hz          <- 'auto'
@@ -84,29 +58,27 @@ winLen_sec     <- 0.0025
 # ------ >>generated some variables from user inputs ----------------------
 # these typically will not change but file names may need slight modifications
 pgVerPrfx <- paste0('pam', pgVer)
-trStr <- paste0(tripStr, '_', recStr)
-fnStr <- paste0('pam', pgVer, '_', tripStr, '_', recStr)
+fnStr <- paste0('pam', pgVer, '_', mission)
 
 # some paths
-path_pg <- paste0(path_analysis, 'pamguard')
+path_pg <- file.path(path_analysis, 'pamguard')
 
 # some file paths
-binDir <- file.path(path_pg, 'binaries', tripStr, 
-                    paste0(pgVerPrfx, '_llharp_banter_', trStr))
-dbFile <- file.path(path_pg, 'databases',  tripStr, 
-                    paste0(pgVerPrfx, '_llharp_banter_', trStr, '.sqlite3'))
-
+binDir <- file.path(path_pg, 'binaries', paste0(pgVerPrfx, '_glider_banter_', 
+                                                mission))
+dbFile <- file.path(path_pg, 'databases', paste0(pgVerPrfx, '_glider_banter_', 
+                                                   mission, '.sqlite3'))
 # encounter times file - phase 3 naming scheme
-encTimesFile <- file.path(path_analysis, 'manualPicks', tripStr, 
-                          paste0(trStr, '_encounterTimes.csv'))
+logFile <- file.path(path_analysis, 'triton', 'merged_logs', 
+                          paste0(mission, '_log_merged.csv'))
 
 # files to be created
-paramFile <- file.path(path_analysis, 'eventDetections', 'parameters', 
-                       paste0(fnStr, '_params.rda'))
-detsFile <- file.path(path_analysis, 'eventDetections', 'dets',
-                      paste0(fnStr, '_dets.rda'))
-detsFiltFile <- file.path(path_analysis, 'eventDetections', 'dets_filtered',
-                          paste0(fnStr, '_detsFilt.rda'))
+paramFile <- file.path(path_analysis, 'classification', 'params', 
+                       paste0(mission, '_params.rda'))
+detsFile <- file.path(path_analysis, 'classification', 'dets',
+                      paste0(mission, '_dets.rda'))
+detsFiltFile <- file.path(path_analysis, 'classification', 'dets_filtered',
+                          paste0(mission, '_detsFilt.rda'))
 
 # ------ PAMpal steps -----------------------------------------------------
 
@@ -135,7 +107,7 @@ if (!file.exists(paramFile)){
 
 if (!file.exists(detsFile)){
   dets <- processPgDetections(fkwPps, mode = 'time', id = paste0(trStr),
-                              grouping = encTimesFile, 
+                              grouping = logFile, 
                               format = '%m/%d/%Y %H:%M')
   # format = '%m/%d/%Y %H:%M:%S')
   # the time format may need to be modified depending on how the csv was made
